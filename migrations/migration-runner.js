dc06698f5d0b6530dd5c00bf341f8e6b950d27e3
Ravshanbek <83048414+Ryan-Karimov@users.noreply.github.com>
2025-02-19 01:22:34 +0500

    Update migration-runner.js

--- migrations/migration-runner.js
+++ migrations/migration-runner.js
@@ -5,7 +5,6 @@ const runMigrations = async () => {
   const umzug = new Umzug({
     migrations: { glob: "migrations/*.js" },
     storage: new SequelizeStorage({ sequelize }),
-    // Передаём объект с queryInterface и Sequelize в контекст
     context: { queryInterface: sequelize.getQueryInterface(), Sequelize },
     logger: console,
   });
